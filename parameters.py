7040794865cf88e5c3aa137710619ec3230b747a
Matteo Tomasini <31040098+mtomasini@users.noreply.github.com>
2020-05-27 14:25:14 -0400

    Added a few comments to code.

--- parameters.py
+++ parameters.py
@@ -1,5 +1,5 @@
 """
-Created: ? 2018
+Created: 2018
 Updated: may 2019
 Author: Matteo Tomasini
 Paper: When does gene flow facilitate evolutionary rescue?
@@ -23,17 +23,16 @@ replicates = 2000
 probability_of_rescue = 0.0
 confidence_interval = 0.0
 
-# structure of the habitat #
+# insert the parameters to test in these lists, and all the combinations of parameters are saved to a file.
 list_of_demes = [2]
-# epoch = 50.
 list_of_epoch = [500]
 list_of_carrying_capacities = [20000]
 list_of_migration_rates = np.logspace(-4, 0, 25)
 list_of_mutation_rates = [1. / list_of_carrying_capacities[0]]
-initial_frequencies = [0.0000]   # These are redefined later, in the loop.
-list_of_growth_rates = [1.5]  # Growth rates must be > 1
-list_of_capacity_ratios = [0.5]
-list_of_migration_ratios = [0.5]
+initial_frequencies = [0.0000]   # These are redefined later, in the loop, as they depend on selection against mutants.
+list_of_growth_rates = [1.5]     # Growth rates must be > 1
+list_of_capacity_ratios = [0.5]  # Asymmetry of carrying capacity (beta in main text)
+list_of_migration_ratios = [0.5] # Asymmetry of migration rates (zeta in main text)
 
 # selection coefficients
 list_of_selection_for = [0.02]    # Selection in favor of mutants in deteriorated environment, > 0
